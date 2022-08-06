766c2641038774f062f336cd83c7eefa6dd5f4fb
Stijn De Preter <stijndepreter@live.be>
2022-08-06 14:52:02 +0200

    delete function added

--- src/app/applications/application.ts
+++ src/app/applications/application.ts
@@ -2,6 +2,7 @@ import { User } from "../security/user";
 import { Vacancy } from "../vacancies/vacancy";
 
 export interface Application {
+    id: number;
     userId:number;
     vacencyId: number;
     motivation:string;
--- src/app/vacancies/vacancy-detail/vacancy-detail.component.ts
+++ src/app/vacancies/vacancy-detail/vacancy-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Application } from 'src/app/applications/application';
 import { Vacancy } from '../vacancy';
 import { VacancyService } from '../vacancy.service';
 
@@ -9,6 +11,9 @@ import { VacancyService } from '../vacancy.service';
   styleUrls: ['./vacancy-detail.component.scss']
 })
 export class VacancyDetailComponent implements OnInit {
+
+  applied: boolean =false;
+
   vacancy: Vacancy = {
     id: 0,
     title: "",
@@ -21,23 +26,62 @@ export class VacancyDetailComponent implements OnInit {
     company: {id: 0,name: "", description: "",picture: ""}
   }
 
+  myApplication: Application | null = null;
+  // {
+  //   userId: 0,
+  //   vacencyId: 0,
+  //   motivation: ""
+  // };
+
+  vacancy$: Subscription = new Subscription();
+  application$: Subscription = new Subscription();
+  deleteApplication$: Subscription = new Subscription();
+  errorMessage: any;
+
   constructor(private vacancyService: VacancyService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     const vacancyId = this.route.snapshot.paramMap.get('id');
     //console.log(this.route.snapshot.paramMap.get('test'));
     if (vacancyId != null) {
-      this.vacancyService.getVacancyById(+vacancyId).subscribe(result => this.vacancy = result);
-      console.log(vacancyId);
-    }
-  }
+      this.vacancy$ = this.vacancyService.getVacancyById(+vacancyId).subscribe(result => this.vacancy = result);
+      this.application$ =this.vacancyService.getMyApplication(+vacancyId).subscribe(result => this.myApplication = result);
+      
+      // console.log(this.myApplication);
 
-  
+      // if (this.myApplication.motivation != ""){
+      //   this.applied = true;
+      //   console.log("alreay applied")
+      // }
+      
+    }
+
+
+  }
 
   applyForVacancy(id: number) {
-    console.log(id)
+    console.log(this.myApplication)
     this.router.navigate(['/applyforvacancy', id]);
   }
+
+  deleteApplication() {
+    console.log("delete")
+    // console.log(this.myApplication.motivation)
+
+    if (this.myApplication != null){
+      this.deleteApplication$ = this.vacancyService.deleteApplication(this.myApplication.id).subscribe(result => {
+      console.log('deleete successfully')
+      this.application$ =this.vacancyService.getMyApplication(this.vacancy.id).subscribe(result => this.myApplication = result);
+    }, error => {
+      //error
+      console.log('deleete failed')
+      this.errorMessage = error.message;
+    });
+
+    }
+    
+  }
+
 
   
 
